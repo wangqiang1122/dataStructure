f045a9d8a0beb80e537119084ea30ace2619d190
wangqiang1122 <wangqiangle@163.com>
2019-06-10 18:18:32 +0800

    链表

--- bethanath/bethanath1.js
+++ bethanath/bethanath1.js
@@ -6,7 +6,7 @@ function linkList() {
         this.data = data;
         this.next = null
     }
-    var length = 0;
+    var length = 0
     var head = null;
     var tail = null;
     // 添加
@@ -19,7 +19,7 @@ function linkList() {
             tail.next = node;
             tail = node;
         }
-        length+=1;
+        length++;
         return true
     }
     //打印出来
@@ -46,6 +46,105 @@ function linkList() {
             }
         }
         return null
+    }
+    // 返回长度
+    this.length = function () {
+        return length
+    }
+    // 返回首节点
+    this.head = function () {
+        return head
+    }
+    // 尾节点
+    this.tail = function () {
+        return tail
+    }
+    // 返回指定索引位置元素
+    this.get = function (index) {
+        var cuur_node = head;
+        var num =0 ;
+        while (cuur_node) {
+            if (num ===index) {
+                return cuur_node
+            }
+            cuur_node = cuur_node.next;
+            num++
+        }
+        return cuur_node
+    }
+    // 在指定位置插入一个元素
+    this.insert = function (index,data) {
+      if (index<0) {
+          throw new Error('不能为负数')
+      } else if (index>length) {
+          throw new Error('不能超过链表长度')
+      }
+      var newNode = new Node(data);
+      if (index==0) {
+          newNode.next = head;
+          head = newNode
+      } else {
+          var preNode = this.get(index-1);
+          newNode.next = preNode.next
+          preNode.next = newNode;
+      }
+      length+=1
+    }
+    // 删除指定元素
+    this.remove = function (index) {
+        if (index<0) {
+            throw new Error('不能为负数')
+        } else if (index>length) {
+            throw new Error('不能超过链表长度')
+        }
+        var preNode,nextNode,delNode;
+        nextNode = this.get(index+1);
+        preNode = this.get(index-1);
+        delNode = preNode.next;
+        if (index==0) {
+            head = null;
+            head = nextNode;
+        } else if (index===length) {
+            preNode.next = null;
+            tail = preNode;
+        } else {
+            preNode.next = nextNode;
+        }
+        length-=1
+        return delNode
+    }
+    // 清空链表
+    this.clear = function () {
+        head = null;
+    }
+    // 删除首节点
+    this.remove_head = function () {
+        var delNode = this.head();
+        if (!delNode) return null
+        head = null;
+        head = delNode.next;
+        length-=1
+        return delNode.data
+    }
+    // 删除尾节点
+    this.remove_tail = function () {
+        var delNode = this.tail()
+        if (!delNode) return null
+        var index = this.indexOf(this.tail().data)
+        var preNode = this.get(index-1);
+        preNode.next = null
+        tail = preNode;
+        length-=1
+        return delNode.data
+    }
+    // 链表是否为空
+    this.isEmpty = function () {
+        if (head) {
+          return false
+        } else {
+            return true
+        }
+
     }
 }
 
